ce7857fa95f5577744c4352736b6d6411df1e696
the-Tiege <thomasjohndunne97@gmail.com>
2024-01-01 19:29:24 +0000

    docstrings added for test_rectangle.py

--- tests/test_rectangle.py
+++ tests/test_rectangle.py
@@ -1,9 +1,32 @@
-import pytest
+"""
+Test Module for Rectangle Class
 
-import getting_started_pytest.shapes as shapes
+This module contains test cases for the 'Rectangle' class in the 'shapes' module.
+The tests cover the calculation of area, perimeter, and equality comparison of 'Rectangle' instances.
+
+Test Functions:
+- test_area(rectangle): Test case for verifying the calculation of the area of a 'Rectangle'.
+- test_perimeter(rectangle): Test case for verifying the calculation of the perimeter of a 'Rectangle'.
+- test_equality(rectangle, other_rectangle): Test case for comparing the equality of two 'Rectangle' instances.
+
+Example Usage:
+
+Run the tests using pytest:
+$ pytest test_rectangle.py
+
+Note: This module assumes the availability of the 'shapes' module and uses pytest for testing.
+"""
+from getting_started_pytest import shapes
 
 
-def test_area(rectangle):
+def test_area(rectangle: shapes.Rectangle):
+    """
+    Test case for verifying the calculation of the area of a 'Rectangle'.
+
+    Parameters:
+    - rectangle: shapes.Rectangle
+                 A 'Rectangle' instance for testing purposes.
+    """
 
     result = rectangle.area()
     expected_result = 10 * 20
@@ -11,7 +34,14 @@ def test_area(rectangle):
     assert result == expected_result
 
 
-def test_perimeter(rectangle):
+def test_perimeter(rectangle: shapes.Rectangle):
+    """
+    Test case for verifying the calculation of the perimeter of a 'Rectangle'.
+
+    Parameters:
+    - rectangle: shapes.Rectangle
+                 A 'Rectangle' instance for testing purposes.
+    """
 
     result = rectangle.perimeter()
     expected_result = (10 * 2) + (20 * 2)
@@ -19,5 +49,14 @@ def test_perimeter(rectangle):
     assert result == expected_result == expected_result
 
 
-def test_equality(rectangle, other_rectangle):
+def test_equality(rectangle: shapes.Rectangle, other_rectangle: shapes.Rectangle):
+    """
+    Test case for comparing the equality of two 'Rectangle' instances.
+
+    Parameters:
+    - rectangle: shapes.Rectangle
+                 A 'Rectangle' instance for testing purposes.
+    - other_rectangle: shapes.Rectangle
+                       Another 'Rectangle' instance for testing purposes.
+    """
     assert rectangle != other_rectangle
